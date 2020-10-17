1deae10f378529ec76f9074ee6b6485c774f5cd1
Chebotov Nikolay <spiritkola@hotmail.com>
2020-10-17 16:00:20 +0300

    Commit

--- components/appmenu.vue
+++ components/appmenu.vue
@@ -2,11 +2,11 @@
     <nav class="green lighten-1">
         <div class="nav-wrapper container">
             <div class="col s12">
-                <router-link to="/" class="brand-logo">LOGO</router-link>
+                <router-link to="/" class="brand-logo">Awesome Russian IT</router-link>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li><router-link to="/">Домашняя</router-link></li>
                     <li><router-link to="/about">О программе</router-link></li>
-                    <li><router-link to="/contacts">Крнтакты</router-link></li>
+                    <li><router-link to="/contacts">Контакты</router-link></li>
                     <li><router-link to="/404">404</router-link></li>
                 </ul>
             </div>
