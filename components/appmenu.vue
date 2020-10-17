1b306f6406d510e0017c3d215e2e5e9ceec6573b
Chebotov Nikolay <spiritkola@hotmail.com>
2020-10-17 22:44:08 +0300

    Commit

--- components/appmenu.vue
+++ components/appmenu.vue
@@ -1,21 +1,31 @@
 ﻿<template>
-    <nav class="green lighten-1">
-        <div class="nav-wrapper container">
-            <div class="col s12">
-                <router-link to="/" class="brand-logo">Awesome Russian IT</router-link>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><router-link to="/">Домашняя</router-link></li>
-                    <li><router-link to="/events">События</router-link></li>
-                    <li><router-link to="/podcasts">Подкасты</router-link></li>
-                    <li><router-link to="/blogs">Блоги</router-link></li>
-                    <li><router-link to="/channels">Каналы</router-link></li>
-                    <!--<li><router-link to="/about">О программе</router-link></li>
-                    <li><router-link to="/contacts">Контакты</router-link></li>
-                    <li><router-link to="/404">404</router-link></li>-->
-                </ul>
-            </div>
+    <header class="mb-4 green lighten-1">
+    <div class="logo-header text-center">
+        <div class="logo container-fluid layout-container">
+            <a href="/"><img src="/assets/logo.svg"></img></a>
+            <p class="d-none d-sm-block">Awesome .NET open source & community resources</p>
         </div>
-    </nav>
+    </div>
+    <b-navbar toggleable="md" type="dark" variant="dark" class="font-weight-bold">
+        <b-container fluid class="layout-container">
+            <div class="flex-grow-1">
+                <b-navbar-toggle target="main-nav-collapse"></b-navbar-toggle>
+                <b-collapse is-nav id="main-nav-collapse">
+                    <b-navbar-nav class="float-md-right">
+                        <b-nav-item href="/">Домашняя</b-nav-item>
+                        <b-nav-item href="/events">События</b-nav-item>
+                        <b-nav-item href="/podcasts">Подкасты</b-nav-item>
+                        <b-nav-item href="/blogs">Блоги</b-nav-item>
+                        <b-nav-item href="/channels">Каналы</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </div>
+            <!--<b-navbar-nav class="align-self-start">
+                <search-box :project-keys="projectKeys"></search-box>
+            </b-navbar-nav>-->
+        </b-container>
+    </b-navbar>
+</header>
 </template>
 
 <!--<div class="logo-header text-center">
