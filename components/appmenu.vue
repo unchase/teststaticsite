b2486984e252ca4008e4f78d9acc13189fe3fe92
Chebotov Nikolay <spiritkola@hotmail.com>
2020-10-26 23:07:27 +0300

    Commit

--- components/appmenu.vue
+++ components/appmenu.vue
@@ -1,28 +1,4 @@
 ﻿<template>
-    <!--<nav class="green lighten-1">
-        <div class="nav-wrapper container">
-            <div class="col s12">
-                <router-link to="/" class="brand-logo">Awesome Russian IT</router-link>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><router-link to="/">Домашняя</router-link></li>
-                    <li><router-link to="/events">События</router-link></li>
-                    <li><router-link to="/podcasts">Подкасты</router-link></li>
-                    <li><router-link to="/blogs">Блоги</router-link></li>
-                    <li><router-link to="/channels">Каналы</router-link></li>
-                </ul>
-            </div>
-        </div>
-    </nav>-->
-    <!--<div>
-        <md-tabs md-sync-route class="md-accent" md-alignment="left">
-            <md-tab id="tab-home" md-icon="home" md-label="Домашняя" to="/" exact><router-view></router-view></md-tab>
-            <md-tab id="tab-events" md-label="События" md-icon="pages" to="/events"><router-view></router-view></md-tab>
-            <md-tab id="tab-events" md-label="Подкасты" md-icon="pages" to="/podcasts"><router-view></router-view></md-tab>
-            <md-tab id="tab-events" md-label="Каналы" md-icon="pages" to="/channels"><router-view></router-view></md-tab>
-            <md-tab id="tab-blogs" md-label="Блоги" md-icon="pages" to="/blogs"><router-view></router-view></md-tab>
-        </md-tabs>
-    </div>-->
-
     <v-toolbar color="orange darken-2"
                dark
                flat
@@ -46,6 +22,7 @@
 
         <template v-slot:extension>
             <v-tabs centered>
+                <v-tab href="/">Домашняя</v-tab>
                 <v-tab href="/events">События</v-tab>
                 <v-tab href="/podcasts">Подкасты</v-tab>
                 <v-tab href="/youtube-channels">YouTube-каналы</v-tab>
